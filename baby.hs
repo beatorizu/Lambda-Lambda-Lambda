9bda524fe654d860bb011865ab30d6e956077a57
beatorizu <betrizaf@gmail.com>
2017-03-21 20:07:34 -0300

    Refactoring double*

--- baby.hs
+++ baby.hs
@@ -1,1 +1,11 @@
+doubleSmallNumber x = if x > 100 then x else doubleMe x
+
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
+
+doubleUs x y = doubleMe x + doubleMe y
+
 doubleMe x = x + x
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
