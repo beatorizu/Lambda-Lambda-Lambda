c5a2e0e95878f95940ac72a41867fd46cacfc2e6
beatorizu <betrizaf@gmail.com>
2017-03-23 20:22:28 -0300

    Add triangles variable

--- baby.hs
+++ baby.hs
@@ -1,3 +1,9 @@
+-- Which right triangle that has integers for all sides and all sides equal to or smaller than 10 has a perimeter of 24?
+triangules = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, sum [a,b,c] == 24 ]
+
+removeNonUppercase :: String -> String
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
 length' list = sum[1 | _ <-list]
 
 doubleSmallNumber x = if x > 100 then x else doubleMe x
