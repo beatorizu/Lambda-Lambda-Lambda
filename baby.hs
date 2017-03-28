088e0181d51d13c62e2b0f372a8062693aae4232
beatorizu <betrizaf@gmail.com>
2017-03-27 23:01:43 -0300

    Add pattern matching functions

--- baby.hs
+++ baby.hs
@@ -1,3 +1,16 @@
+-- Pattern matching
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
 -- Which right triangle that has integers for all sides and all sides equal to or smaller than 10 has a perimeter of 24?
 triangules = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, sum [a,b,c] == 24 ]
 
