89da421992cc7648e841f015d5825501f1780c43
beatorizu <betrizaf@gmail.com>
2017-03-30 22:27:33 -0300

    Implement some root
    Rewriting some functions using pattern function
    and recursive calls.

--- functional_functions.hs
+++ functional_functions.hs
@@ -39,3 +39,30 @@ fib :: Int -> Int
 fib 1 = 1
 fib 2 = 1
 fib n = fib (n - 1) + fib (n - 2)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- Take fst, snd and trd from triple :3
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+-- Writing head func
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+-- Writing length func
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
