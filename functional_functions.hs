b20e9774e5468823f72f9c2bc7089b7b47bcc125
beatorizu <betrizaf@gmail.com>
2017-04-01 13:01:46 -0300

    Add Recursive MDC

--- functional_functions.hs
+++ functional_functions.hs
@@ -63,6 +63,12 @@ length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
 
+-- Writing sum func
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
+
+-- Recursive MDC - https://github.com/jhoonb/haskell-exemplos/blob/master/exemplos/mdc.hs
+mdc :: Integral a => a -> a -> a
+mdc m n | m `mod` n == 0 = n
+        | m `mod` n /= 0 = mdc n (m `mod` n)
