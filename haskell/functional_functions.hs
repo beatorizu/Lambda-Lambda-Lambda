849eabfef6d01cbb9ca69e4d976176d4a129cd70
beatorizu <betrizaf@gmail.com>
2017-07-05 18:41:07 -0300

    Add pattern matching to return two first elements

--- haskell/functional_functions.hs
+++ haskell/functional_functions.hs
@@ -60,3 +60,9 @@ mdc m n | m `mod` n == 0 = n
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital t@(x:xs) = "The first letter of " ++ t ++ " is " ++ [x]
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
