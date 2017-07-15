6d06675b72c1e432a79bfc97e4de50ace156d2f2
beatorizu <betrizaf@gmail.com>
2017-07-15 14:43:46 -0300

    Move recursive functions to recursion

--- haskell/functional_functions.hs
+++ haskell/functional_functions.hs
@@ -11,19 +11,6 @@ lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"
 
-reverse_number :: Int -> Int -> Int
-reverse_number n r | n < 10 = r * 10 + n
-                   | n >= 10 = reverse_number (n `div` 10) ((10 * r) + (n `mod` 10))
-
-sum_digit :: Int -> Int -> Int
-sum_digit n r | n < 10 = r + n `mod` 10
-              | n >= 10 = sum_digit (n `div` 10) (r + n `mod` 10)
-
-fib :: Int -> Int
-fib 1 = 1
-fib 2 = 1
-fib n = fib (n - 1) + fib (n - 2)
-
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
@@ -41,21 +28,6 @@ third (_, _, z) = z
 head' :: [a] -> a
 head' xs = case xs of [] -> error "Can't call head on an empty list, dummy!"
                       (x:_) -> x
-
--- Writing length func
-length' :: (Num b) => [a] -> b
-length' [] = 0
-length' (_:xs) = 1 + length' xs
-
--- Writing sum func
-sum' :: (Num a) => [a] -> a
-sum' [] = 0
-sum' (x:xs) = x + sum' xs
-
--- Recursive MDC - https://github.com/jhoonb/haskell-exemplos/blob/master/exemplos/mdc.hs
-mdc :: Integral a => a -> a -> a
-mdc m n | m `mod` n == 0 = n
-        | m `mod` n /= 0 = mdc n (m `mod` n)
 
 -- Return capital
 capital :: String -> String
