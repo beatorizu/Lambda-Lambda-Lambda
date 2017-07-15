f1bc152d509ebdb43e2ce1c3b139e0d93bbc1ee0
beatorizu <betrizaf@gmail.com>
2017-07-15 14:21:38 -0300

    Add comments

--- haskell/functional_functions.hs
+++ haskell/functional_functions.hs
@@ -57,46 +57,54 @@ mdc :: Integral a => a -> a -> a
 mdc m n | m `mod` n == 0 = n
         | m `mod` n /= 0 = mdc n (m `mod` n)
 
+-- Return capital
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital t@(x:xs) = "The first letter of " ++ t ++ " is " ++ [x]
 
+-- Describe list length
 tell :: (Show a) => [a] -> String
 tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
 
+-- Tell something about your BMI
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
     | bmi <= shinny = "You're underweight, you emo, you!"
     | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | bmi <= fat = "You're fat! Lose some weight, fatty!"
-    | otherwise   = "You're a whale, congratulations!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
     where bmi = weight / height ^ 2
           shinny = 18.5
           normal = 25.0
           fat = 30.0
 
+-- Return max'
 max' :: (Ord a) => a -> a -> a
 max' a b
     | a > b     = a
     | otherwise = b
 
+-- Return Ordering
 myCompare :: (Ord a) => a -> a -> Ordering
 a `myCompare` b
     | a > b     = GT
     | a == b    = EQ
     | otherwise = LT
 
+-- Return initials names
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
     where (f:_) = firstname
           (l:_) = lastname
 
+-- Calculate list of BMIs
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
 
+-- Calculate cylinder area
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
     let
