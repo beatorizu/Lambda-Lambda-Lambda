8f9468d3d4a061ded13341c45c364b2258c8a82a
beatorizu <betrizaf@gmail.com>
2017-07-16 15:17:10 -0300

    Repeat a item
    To infinity and beyound

--- haskell/recursion.hs
+++ haskell/recursion.hs
@@ -48,3 +48,7 @@ take' n (x:xs) = x : take' (n-1) xs
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
+
+-- Repeat a item
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
