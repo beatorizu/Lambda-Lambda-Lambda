3544842ce2e389f053d59811b41f486ed80513c9
beatorizu <betrizaf@gmail.com>
2017-07-16 13:15:17 -0300

    Reverse a list

--- haskell/recursion.hs
+++ haskell/recursion.hs
@@ -43,3 +43,8 @@ take' n _
     | n <= 0   = []
 take' _ []     = []
 take' n (x:xs) = x : take' (n-1) xs
+
+-- Reverse a list
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
