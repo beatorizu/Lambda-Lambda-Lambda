4f4849aff3e047794bd4beeab5cf277de8e5edad
beatorizu <betrizaf@gmail.com>
2017-11-13 22:07:44 -0200

    Add recursive :3

--- haskell/functional_functions.hs
+++ haskell/functional_functions.hs
@@ -11,6 +11,19 @@ lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"
 
+reverse_number :: Int -> Int -> Int
+reverse_number n r | n < 10 = r * 10 + n
+                   | n >= 10 = reverse_number (n `div` 10) ((10 * r) + (n `mod` 10))
+
+sum_digit :: Int -> Int -> Int
+sum_digit n r | n < 10 = r + n `mod` 10
+              | n >= 10 = sum_digit (n `div` 10) (r + n `mod` 10)
+
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
@@ -26,67 +39,24 @@ third (_, _, z) = z
 
 -- Writing head func
 head' :: [a] -> a
-head' xs = case xs of [] -> error "Can't call head on an empty list, dummy!"
-                      (x:_) -> x
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
 
--- Return capital
+-- Writing length func
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+-- Writing sum func
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+-- Recursive MDC - https://github.com/jhoonb/haskell-exemplos/blob/master/exemplos/mdc.hs
+mdc :: Integral a => a -> a -> a
+mdc m n | m `mod` n == 0 = n
+        | m `mod` n /= 0 = mdc n (m `mod` n)
+
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital t@(x:xs) = "The first letter of " ++ t ++ " is " ++ [x]
-
--- Describe list elemets
-tell :: (Show a) => [a] -> String
-tell [] = "The list is empty"
-tell (x:[]) = "The list has one element: " ++ show x
-tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
-tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
-
--- Tell something about your BMI
-bmiTell :: (RealFloat a) => a -> a -> String
-bmiTell weight height
-    | bmi <= shinny = "You're underweight, you emo, you!"
-    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
-    | otherwise     = "You're a whale, congratulations!"
-    where bmi = weight / height ^ 2
-          shinny = 18.5
-          normal = 25.0
-          fat = 30.0
-
--- Return max'
-max' :: (Ord a) => a -> a -> a
-max' a b
-    | a > b     = a
-    | otherwise = b
-
--- Return Ordering
-myCompare :: (Ord a) => a -> a -> Ordering
-a `myCompare` b
-    | a > b     = GT
-    | a == b    = EQ
-    | otherwise = LT
-
--- Return initials names
-initials :: String -> String -> String
-initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
-    where (f:_) = firstname
-          (l:_) = lastname
-
--- Calculate list of BMIs
-calcBmis :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
-
--- Calculate cylinder area
-cylinder :: (RealFloat a) => a -> a -> a
-cylinder r h =
-    let
-        sideArea = 2 * pi * r * h
-        topArea = pi * r ^2
-      in sideArea + 2 * topArea
-
--- Describe list length
-describeList :: [a] -> String
-describeList xs = "The list is " ++ what xs
-    where what [] = "empty."
-          what [x] = "a singleton list."
-          what xs = "a longer list."
